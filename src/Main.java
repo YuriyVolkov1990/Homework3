93cda36b8bae5addd7efd45660d65fa731b07a7a
Professional <new99kpicem@gmail.com>
2023-02-05 00:26:59 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int i = 1;
+        System.out.println("Значение переменной i с типом integer равно " + i);
+        byte b = 2;
+        System.out.println("Значение переменной i с типом byte равно " + b);
+        short s = 3;
+        System.out.println("Значение переменной s с типом short равно " + s);
+        long l = 4;
+        System.out.println("Значение переменной l с типом long равно " + l);
+        float f = 5;
+        System.out.println("Значение переменной f с типом float равно " + f);
+        double d = 6;
+        System.out.println("Значение переменной d с типом double равно " + d);
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+    }
+}
